c9df5dd1e22d1c192ffab6f0bf3c599d863e6dbf
YasuhiroSuzu <y_suzuki@gacci.jp>
2024-02-14 19:34:09 +0900

    APIキーの設定に疲れた

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -2,17 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// 環境変数から Firebase の設定を読み込む
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCGXB0QAxH4pGZwIzKMv-AonhtmPYaCy8M",
+  authDomain: "line-clone-app-b80d8.firebaseapp.com",
+  projectId: "line-clone-app-b80d8",
+  storageBucket: "line-clone-app-b80d8.appspot.com",
+  messagingSenderId: "625248172827",
+  appId: "1:625248172827:web:36c62515d04c875a6b71be",
+  measurementId: "G-HFFR9QSJSQ",
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
